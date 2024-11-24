53dd3240e6ab72501ff354a605f0a3ada676905f
ricardo <ricardocastroparmezan@gmail.com>
2024-11-24 15:04:21 -0300

    Cadastro de Contatos

---
+++ ControleDeContatos/Repository/ContatoRepositorio.cs
@@ -0,0 +1,23 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+
+namespace ControleDeContatos.Repository;
+
+public class ContatoRepositorio : IContatoRepositorio
+{
+    private readonly BancoContext _bancoContext;
+    public ContatoRepositorio(BancoContext bancoContext)
+    {
+        _bancoContext = bancoContext;
+    }
+    public ContatoModel Adicionar(ContatoModel model)
+    {
+        _bancoContext.Contatos.Add(model);
+        _bancoContext.SaveChanges();
+        return model;
+    }
+    public List<ContatoModel> BuscarTodos()
+    {
+        return _bancoContext.Contatos.ToList();
+    }
+}
---
+++ ControleDeContatos/Repository/IContatoRepositorio.cs
@@ -0,0 +1,9 @@
+﻿using ControleDeContatos.Models;
+
+namespace ControleDeContatos.Repository;
+
+public interface IContatoRepositorio
+{
+    ContatoModel Adicionar(ContatoModel model);
+    List<ContatoModel> BuscarTodos();
+}
