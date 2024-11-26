65fe7263804430826205e3f41a3fec81dff1692a
ricardo <ricardocastroparmezan@gmail.com>
2024-11-25 22:37:18 -0300

    implements editing

--- ControleDeContatos/Repository/ContatoRepositorio.cs
+++ ControleDeContatos/Repository/ContatoRepositorio.cs
@@ -10,14 +10,35 @@ public class ContatoRepositorio : IContatoRepositorio
     {
         _bancoContext = bancoContext;
     }
-    public ContatoModel Adicionar(ContatoModel model)
+    public ContatoModel Adicionar(ContatoModel contato)
     {
-        _bancoContext.Contatos.Add(model);
+        _bancoContext.Contatos.Add(contato);
         _bancoContext.SaveChanges();
-        return model;
+        return contato;
+    }
+    public ContatoModel Atualizar(ContatoModel contato)
+    {
+        var contatoDb = this.BuscarPorId(contato.Id);
+        if (contatoDb == null)
+        {
+            throw new Exception("Ocorre um erro ao atualizar o contato");
+        }
+
+        contatoDb.Nome = contato.Nome;
+        contatoDb.Email = contato.Email;
+        contatoDb.Celular = contato.Celular;
+
+        _bancoContext.Update(contatoDb);
+        _bancoContext.SaveChanges();
+        return contatoDb;
     }
     public List<ContatoModel> BuscarTodos()
     {
         return _bancoContext.Contatos.ToList();
+    }
+
+    public ContatoModel BuscarPorId(int id)
+    {
+        return _bancoContext.Contatos.FirstOrDefault(item => item.Id == id)!;
     }
 }
--- ControleDeContatos/Repository/IContatoRepositorio.cs
+++ ControleDeContatos/Repository/IContatoRepositorio.cs
@@ -5,5 +5,7 @@ namespace ControleDeContatos.Repository;
 public interface IContatoRepositorio
 {
     ContatoModel Adicionar(ContatoModel model);
+    ContatoModel Atualizar(ContatoModel model);
+    ContatoModel BuscarPorId(int id);
     List<ContatoModel> BuscarTodos();
 }
