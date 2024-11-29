5a0440d5209405e345281f9a4acfe74451ffb315
ricardo <ricardoparmezancastro.dev@gmail.com>
2024-11-28 23:27:10 -0300

    Criando Cadastro de Usuários 2/2

--- ControleDeContatos/Controllers/UsuarioController.cs
+++ ControleDeContatos/Controllers/UsuarioController.cs
@@ -16,4 +16,29 @@ public class UsuarioController : Controller
         List<UsuarioModel> usuarios = _usuarioRepositorio.BuscarTodos();
         return View(usuarios);
     }
+    public IActionResult Criar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Criar(UsuarioModel usuario)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepositorio.Adicionar(usuario);
+                TempData["MensagemSucesso"] = "Usuário cadastrado com sucesso!";
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+        catch (Exception ex)
+        {
+            TempData["MensagemErro"] = $"Não conseguimos cadastrar o Usuário!,por causa:{ex.Message}";
+            return RedirectToAction("Index");
+        }
+    }
 }
