2a736bca443ff49d3654230a5d949b12e914e425
ricardo <ricardocastroparmezan@gmail.com>
2024-11-28 23:27:10 -0300

    Criando Cadastro de Usuários 2/2

--- ControleDeContatos/Models/UsuarioModel.cs
+++ ControleDeContatos/Models/UsuarioModel.cs
@@ -1,13 +1,25 @@
 ﻿using ControleDeContatos.Enums;
+using System.ComponentModel.DataAnnotations;
 namespace ControleDeContatos.Models;
 
 public class UsuarioModel
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Digite o Nome do Usuário")]
     public string Nome { get; set; }
-    public string Login { get; set; }   
+
+    [Required(ErrorMessage = "Digite o Login do Usuário")]
+    public string Login { get; set; }
+
+    [Required(ErrorMessage = "Digite o Email do Usuário")]
+    [EmailAddress(ErrorMessage = "Digite um Email Valido")]
     public string Email { get; set; }
+
+    [Required(ErrorMessage = "Digite a Senha do Usuário")]
     public string Senha { get; set; }
+
+    [Required(ErrorMessage = "Digite o Perfil do Usuário")]
     public PerfilEnum Perfil { get; set; }
     public DateTime DataCadastro { get; set; }
     public DateTime? DataAtualizacao { get; set; }
